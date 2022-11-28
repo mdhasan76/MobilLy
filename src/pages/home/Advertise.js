3f3a57213b609eb9f19bfe0b9bc0b230cfc78679
Md Hasan <mdhasan8064@gmail.com>
2022-11-28 20:39:00 +0600

    finish advertize items

--- src/pages/home/Advertise.js
+++ src/pages/home/Advertise.js
@@ -22,7 +22,7 @@ const Advertise = () => {
         <div >
             {advertizeItems.length > 0 &&
                 <div>
-
+                    <h1 className='text-3xl mb-10 text-center sm:text-6xl font-semibold pb-7'>Advertize Items</h1>
                     <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 mb-10 p-3'>
                         {
 
